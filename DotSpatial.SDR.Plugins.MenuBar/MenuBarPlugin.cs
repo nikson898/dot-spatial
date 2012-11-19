1b9f9ed4b13a67f44d276e4cd7923c25a24815b9
Justin Penka <jpenka@gmail.com>
2012-11-19 15:53:12 -0600

    added in basic menubar functionality

---
+++ DotSpatial.SDR.Plugins.MenuBar/MenuBarPlugin.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MenuBarPlugin.cs" company="">
+//
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using DotSpatial.Controls;
+using DotSpatial.Controls.Header;
+using DotSpatial.Data;
+using DotSpatial.SDR.Plugins.MenuBar.Properties;
+using Msg = DotSpatial.SDR.Plugins.MenuBar.MessageStrings;
+using DotSpatial.Topology;
+
+namespace DotSpatial.SDR.Plugins.MenuBar
+{
+    public class MenuBarPlugin : Extension
+    {
+        #region Constants and Fields
+
+        private const string FileMenuKey = HeaderControl.ApplicationMenuKey;
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Activate()
+        {
+            IHeaderControl header = App.HeaderControl;
+            header.Add(new RootItem(FileMenuKey, MessageStrings.File) { SortOrder = -20 });
+            header.Add(new SimpleActionItem(FileMenuKey, Msg.File_Print, PrintLayout_Click) { GroupCaption = HeaderControl.ApplicationMenuKey, SortOrder = 40, SmallImage = Resources.printer_16x16, LargeImage = Resources.printer_32x32 });
+            header.Add(new SimpleActionItem(FileMenuKey, Msg.File_Reset_Layout, ResetLayout_Click) { GroupCaption = HeaderControl.ApplicationMenuKey, SortOrder = 200, SmallImage = Resources.layout_delete_16x16, LargeImage = Resources.layout_delete_32x32 });
+            header.Add(new SimpleActionItem(FileMenuKey, Msg.File_Exit, Exit_Click) { GroupCaption = HeaderControl.ApplicationMenuKey, SortOrder = 5000, });
+            base.Activate();
+        }
+
+        public override void Deactivate()
+        {
+            App.HeaderControl.RemoveAll();
+            base.Deactivate();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the PrintLayout control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void PrintLayout_Click(object sender, EventArgs e)
+        {
+            using (LayoutForm layout = new LayoutForm())
+            {
+                layout.MapControl = App.Map as Map;
+                layout.ShowDialog();
+            }
+        }
+
+        private void ResetLayout_Click(object sender, EventArgs e)
+        {
+            App.DockManager.ResetLayout();
+        }
+        #endregion
+    }
+}
